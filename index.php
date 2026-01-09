db41cce78d7b53786fa57f5aabb5459b8ab7675e
kuroi-hime <kuroihime095@gmail.com>
2026-01-09 20:49:23 +0100

    page de redirection.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // 1. L'UNIQUE REQUIRE NÉCESSAIRE
+    require __DIR__ . '/assets/vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=assets/php/pages/authentification.php">
+    <title>Ma Bagnole</title>
+</head>
+</html>
