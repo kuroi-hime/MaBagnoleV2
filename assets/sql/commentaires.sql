cecd6d801abc0979f2240006b106c6a523204205
kuroi-hime <kuroihime095@gmail.com>
2026-01-09 20:53:05 +0100

    sql: définition et insertion des données.

--- assets/sql/commentaires.sql
+++ assets/sql/commentaires.sql
@@ -1,11 +1,11 @@
 INSERT INTO commentaires (contenu, note, statut, client_id, vehicule_id, article_id) VALUES 
-('Super voiture !', 5, 'Validé', 2, 1, NULL),
-('Article très utile', 4, 'Validé', 3, NULL, 1),
-('Un peu cher', 3, 'Validé', 4, 3, NULL),
-('Merci pour les conseils', 5, 'Validé', 5, NULL, 2),
-('Boite fluide', 5, 'Validé', 6, 2, NULL),
-('Trop de pub', 2, 'Refusé', 7, NULL, 3),
-('Parfait pour ma famille', 5, 'Validé', 8, 9, NULL),
-('Pas aimé la couleur', 3, 'Validé', 9, 5, NULL),
-('Magnifique road trip', 5, 'Validé', 10, NULL, 4),
-('Service client top', 4, 'Validé', 2, 4, NULL);
+('Super voiture !', 5, 'added', 2, 1, NULL),
+('Article très utile', 4, 'added', 3, NULL, 1),
+('Un peu cher', 3, 'added', 4, 3, NULL),
+('Merci pour les conseils', 5, 'added', 5, NULL, 2),
+('Boite fluide', 5, 'added', 6, 2, NULL),
+('Trop de pub', 2, 'deleted', 7, NULL, 3),
+('Parfait pour ma famille', 5, 'added', 8, 9, NULL),
+('Pas aimé la couleur', 3, 'added', 9, 5, NULL),
+('Magnifique road trip', 5, 'added', 10, NULL, 4),
+('Service client top', 4, 'added', 2, 4, NULL);
